e8cc6cefe62abe87dfd344f74fac0e01469a3e60
Johnny <hellojinqiang@gmail.com>
2021-12-08 14:05:36 +0800

    fix(srs/rtc2rtmp): shared rtmp packet for dispatch_audio.

--- trunk/src/app/srs_app_rtc_source.cpp
+++ trunk/src/app/srs_app_rtc_source.cpp
@@ -1827,13 +1827,31 @@ class SimulcastBridgeAdapter: public SrsRtmpFromRtcBridge {
     std::map<uint32_t, SrsRtmpFromRtcBridge*> bridges_;
     std::vector<SrsLiveSource*> sources_;
 
-    srs_error_t dispatch_audio(SrsCommonMessage* out_rtmp) {
+    SrsCommonMessage copy;
+
+    srs_error_t dispatch_audio(SrsCommonMessage* rtmp) {
         srs_error_t err = srs_success;
+        if (!rtmp->payload) {
+            srs_error("rtmp audio packet is nullptr, skip it");
+            return err;
+        }
         for (auto& source: sources_) {
-            if ((err = source->on_audio(out_rtmp)) != srs_success) {
+            assert(rtmp->payload);
+
+            // backup
+            // @see SrsSharedPtrMessage::create(SrsCommonMessage* msg)
+            copy.create_payload(rtmp->size);
+            copy.size = rtmp->size;
+            memcpy(copy.payload, rtmp->payload, rtmp->size);
+
+            if ((err = source->on_audio(rtmp)) != srs_success) {
                 err = srs_error_wrap(err, "source[%p] on audio", source);
                 break;
             }
+
+            // restore
+            std::swap(rtmp->payload, copy.payload);
+            std::swap(rtmp->size, copy.size);
         }
         return err;
     }
